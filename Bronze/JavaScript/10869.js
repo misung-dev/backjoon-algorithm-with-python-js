06d5c9a6a8d8740d6ec35cd3a8ce8286f0c5f341
misung.dev <misung.dev@gmail.com>
2024-05-31 12:57:38 +0900

    solve: 사칙연산

---
+++ Bronze/JavaScript/10869.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("/dev/stdin");
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
