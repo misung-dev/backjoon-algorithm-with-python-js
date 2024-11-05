b91839b16d4780f4bbba51da640cd5d4c608127b
misung.dev <misung.dev@gmail.com>
2024-11-06 03:36:01 +0900

    solve: 그대로 출력하기

---
+++ Bronze_python/11718.py
@@ -0,0 +1,17 @@
+# 그대로 출력하기
+
+while True:
+  try: 
+    a = input()
+    print(a)
+  except:
+    break
+
+
+# try-except 구문은 파이썬에서 예외 처리를 위해 사용되는 구문
+
+# 기본 구문                                      
+# try:
+# 	실행할 코드
+# except:
+# 	예외가 발생할 때 실행할 코드
