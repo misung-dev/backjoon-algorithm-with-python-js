7cd70b1210244a0658995c0ec48af7a196dd17c8
misung.dev <misung.dev@gmail.com>
2025-05-04 22:49:07 +0900

    solve: 골드바흐 파티션

---
+++ Silver_python/17103.py
@@ -0,0 +1,24 @@
+# 골드바흐 파티션
+
+import sys
+
+input = sys.stdin.readline
+
+primes = [True for _ in range(1000001)]
+
+for i in range(2, 1000001):
+  if primes[i] == True:
+    for j in range(i*2, 1000001, i):
+      primes[j] = False
+
+T = int(input())
+
+for i in range(T):
+  n = int(input())
+  partition = 0
+  
+  for num in range(2, n // 2 + 1):
+    if primes[num] and primes[n - num]:
+      partition += 1
+  
+  print(partition)
