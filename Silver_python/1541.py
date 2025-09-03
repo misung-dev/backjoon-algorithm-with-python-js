65bbb1e58190dae3654e47fb9ae5d2ac01aa4402
misung.dev <misung.dev@gmail.com>
2025-09-04 00:15:25 +0900

    solve2: 잃어버린 괄호

--- Silver_python/1541.py
+++ Silver_python/1541.py
@@ -1,45 +1,12 @@
 # 잃어버린 괄호 (그리디 알고리즘)
-expression = str(input())
-arr = []
-value = ''
+expr = input()
 
-# 숫자와 연산자 분리
-for i in range(len(expression)):
-  if (expression[i] == '-') :
-    arr.append(int(value)) 
-    arr.append('-')
-    i += 1
-    value = ''  
+groups = expr.split('-')
+sums = [sum(map(int, g.split('+'))) for g in groups]
+answer = sums[0] - sum(sums[1:])
+print(answer)
 
-  elif (expression[i] == '+'):
-    arr.append(int(value))    
-    arr.append('+')
-    i += 1
-    value = ''  
-
-  else:
-    value += expression[i]
-
-arr.append(int(value))
-
-# 연산자에 따라 계산
-sum = 0
-j = 1
-
-while j < len(arr) - 1:
-  if (arr[j] == '+'):
-    sum = arr[j-1] + arr[j+1]
-    del arr[j-1:j+2]
-    arr.insert(j-1, sum)
-    j -= 1
-  else:
-    j += 1 
-
-# 순회하면서 뺄셈
-total = arr[0]
-
-for k in range(2, len(arr), 2):
-  total -= arr[k]
-
-# 출력
-print(total)
+# ex) 55-50+40+70-100
+# ['55', '50+40+70', '100']
+# [55, 160, 100]
+# -205
